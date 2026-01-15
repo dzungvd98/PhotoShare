862a5661ff81d8e2702d120bdc62f59bdb1dfc98
dzungvd98 <yungvd98@gmail.com>
2026-01-15 22:29:48 +0700

    fix: folder  not found image upload

--- src/main/java/com/dev/photoshare/config/WebConfig.java
+++ src/main/java/com/dev/photoshare/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.dev.photoshare.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,22 +8,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.upload-dir}")
+    private String uploadDir;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Cho phép tất cả endpoint
+        registry.addMapping("/**")
                 .allowedOriginPatterns(
-                    "http://localhost:3000",
-                    "http://localhost:5873",
-                    "https://*.phao.id.vn"
+                        "http://localhost:3000",
+                        "http://localhost:5873",
+                        "https://*.phao.id.vn"
                 )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // Các method được phép
-                .allowedHeaders("*") // Cho phép tất cả header
-                .allowCredentials(true); // Credentials cho trường hợp này
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/images/**")
-                .addResourceLocations("file:upload/images/"); // đường dẫn thực tế
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + uploadDir + "/");
     }
 }
